e09003f47f50d2c35f91765ca128b3bb1353b0f3
Tevive <togok32012@dosbee.com>
2026-06-04 17:17:36 +0200

    feat: expose auth + scan + download IPC

--- artifacts/tevive-desktop/src/preload.ts
+++ artifacts/tevive-desktop/src/preload.ts
@@ -1,46 +1,43 @@
 import { contextBridge, ipcRenderer } from "electron";
 
 contextBridge.exposeInMainWorld("tevive", {
-  // App info
   getVersion: () => ipcRenderer.invoke("get-app-version"),
-  getDownloadsDir: () => ipcRenderer.invoke("get-downloads-dir"),
   platform: process.platform,
+
+  // Settings
+  getSettings: () => ipcRenderer.invoke("get-settings"),
+  saveSettings: (s: unknown) => ipcRenderer.invoke("save-settings", s),
+  pickDownloadFolder: () => ipcRenderer.invoke("pick-download-folder"),
+
+  // Auth
+  register: (d: unknown) => ipcRenderer.invoke("auth-register", d),
+  login: (d: unknown) => ipcRenderer.invoke("auth-login", d),
+  updateUsername: (d: unknown) => ipcRenderer.invoke("auth-update-username", d),
 
   // Games
   getGames: () => ipcRenderer.invoke("get-games"),
-  addGame: (game: unknown) => ipcRenderer.invoke("add-game", game),
+  addGame: (g: unknown) => ipcRenderer.invoke("add-game", g),
   updateGame: (id: string, patch: unknown) => ipcRenderer.invoke("update-game", id, patch),
   deleteGame: (id: string) => ipcRenderer.invoke("delete-game", id),
 
-  // File pickers
+  // Upload + scan
   pickThumbnail: () => ipcRenderer.invoke("pick-thumbnail"),
-  pickFileToScan: () => ipcRenderer.invoke("pick-file-to-scan"),
-
-  // GitHub Release
+  pickGameFile: () => ipcRenderer.invoke("pick-game-file"),
   resolveGithubRelease: (url: string) => ipcRenderer.invoke("resolve-github-release", url),
+  scanAndPublish: (d: unknown) => ipcRenderer.invoke("scan-and-publish", d),
+  getScanHistory: () => ipcRenderer.invoke("get-scan-history"),
+
+  // Scan progress listener
+  onScanProgress: (cb: (d: unknown) => void) => ipcRenderer.on("scan-progress", (_e, d) => cb(d)),
+  offScanProgress: () => ipcRenderer.removeAllListeners("scan-progress"),
 
   // Downloads
   startDownload: (opts: unknown) => ipcRenderer.invoke("start-download", opts),
   cancelDownload: (id: string) => ipcRenderer.invoke("cancel-download", id),
-  getActiveDownloads: () => ipcRenderer.invoke("get-active-downloads"),
-  onDownloadProgress: (cb: (data: unknown) => void) => {
-    ipcRenderer.on("download-progress", (_e, data) => cb(data));
-  },
-  offDownloadProgress: () => {
-    ipcRenderer.removeAllListeners("download-progress");
-  },
-
-  // Scanner
-  scanFile: (filePath: string) => ipcRenderer.invoke("scan-file", filePath),
-  getScanHistory: () => ipcRenderer.invoke("get-scan-history"),
-  onScanUpdate: (cb: (data: unknown) => void) => {
-    ipcRenderer.on("scan-update", (_e, data) => cb(data));
-  },
-  offScanUpdate: () => {
-    ipcRenderer.removeAllListeners("scan-update");
-  },
+  onDownloadProgress: (cb: (d: unknown) => void) => ipcRenderer.on("download-progress", (_e, d) => cb(d)),
+  offDownloadProgress: () => ipcRenderer.removeAllListeners("download-progress"),
 
   // System
-  openInFolder: (filePath: string) => ipcRenderer.invoke("open-in-folder", filePath),
-  launchGame: (filePath: string) => ipcRenderer.invoke("launch-game", filePath),
+  openInFolder: (p: string) => ipcRenderer.invoke("open-in-folder", p),
+  launchGame: (p: string) => ipcRenderer.invoke("launch-game", p),
 });
